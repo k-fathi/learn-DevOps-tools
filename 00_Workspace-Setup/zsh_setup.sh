cfe63299ba810850280dd11c71957a2ca85417b8
Karim Fathy <karimfathy.devops@gmail.com>
2026-08-10 18:17:24 +0300

    Upload the Zsh_setup.sh Script

--- 00_Workspace-Setup/zsh_setup.sh
+++ 00_Workspace-Setup/zsh_setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 echo "Checking Package Manager and installing dependencies..."
-
 if command -v apt &> /dev/null; then
     sudo apt update
     sudo apt install zsh curl git sed -y
 
 elif command -v dnf &> /dev/null; then
-    sudo dnf install zsh curl git sed util-linux-user -y
+    sudo dnf install zsh curl git sed util-linux-user --allowerasing -y
 
 elif command -v yum &> /dev/null; then
     sudo yum install zsh curl git sed util-linux-user -y
@@ -24,6 +23,9 @@ echo "Installing Oh My Zsh..."
 RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Installing ZSH Plugins..."
+rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
