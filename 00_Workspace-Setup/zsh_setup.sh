c69e4fc4c21906ad6c868404e1d282ab64bdc2c2
Karim Fathy <karimfathy.devops@gmail.com>
2026-08-10 18:08:13 +0300

    Upload the Zsh_setup.sh Script

--- 00_Workspace-Setup/zsh_setup.sh
+++ 00_Workspace-Setup/zsh_setup.sh
@@ -1,18 +1,15 @@
 sudo apt update
-sudo apt install zsh -y
-
-zsh --version
-which zsh
+sudo apt install zsh curl git -y
 
 chsh -s $(which zsh)
 
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-sh -c "$(curl -fsSL [https://install.ohmyz.sh/](https://install.ohmyz.sh/))" || \
-sh -c "$(wget -O- [https://install.ohmyz.sh/](https://install.ohmyz.sh/))" || \
-sh -c "$(fetch -o - [https://install.ohmyz.sh/](https://install.ohmyz.sh/))"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+sed -i 's/^plugins=(git)/plugins=(git docker kubectl helm docker-compose dotenv zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 
-echo "ZSH_THEME="robbyrussell"" >> ~/.zshrc
-echo "plugins=(git docker kubectl helm docker-compose dotenv zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="robbyrussell"/' ~/.zshrc
 
 source ~/.zshrc
